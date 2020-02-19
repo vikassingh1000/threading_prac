04c1043981dc466f0605ee818321ac2117be3e18
Vikas Singh <vikassingh1000@outlook.com>
2020-02-19 21:19:25 +0530

    All Files

--- src/main/java/te/ParseMT103FromFileExample.java
+++ src/main/java/te/ParseMT103FromFileExample.java
@@ -1,6 +1,13 @@
 package te;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import com.prowidesoftware.swift.model.field.Field;
 import com.prowidesoftware.swift.model.field.Field20;
@@ -16,22 +23,49 @@ public class ParseMT103FromFileExample {
          * Parse from File could also be used here
          */
         MT103 mt = MT103.parse(Lib.readResource("mt103", null));
-
+        Map<String,String> data = new LinkedHashMap<>();
         /*
          * Print header information
          */
-        System.out.println("Sender: "+mt.getSender());
-        System.out.println("Receiver: "+mt.getReceiver());
+        data.put("sender", mt.getSender());
+        data.put("receiver", mt.getReceiver());
 
         /*
          * Print details of a specific fields
          */
         Field20 ref = mt.getField20();
-        System.out.println(Field.getLabel(ref.getName(), mt.getMessageType(), null) + ": " + ref.getComponent(Field20.REFERENCE));
+        data.put(Field.getLabel(ref.getName(), mt.getMessageType(), null), ref.getComponent(Field20.REFERENCE));
 
         Field32A f = mt.getField32A();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        System.out.println("Value Date: "+sdf.format(f.getDateAsCalendar().getTime()));
-        System.out.println("Amount: "+f.getCurrency()+" "+f.getAmount());
+        data.put("Value Date", sdf.format(f.getDateAsCalendar().getTime()));
+        data.put("Amount", f.getCurrency()+" "+f.getAmount());
+
+        Charset utf8 = StandardCharsets.UTF_8;
+        String headers = data.keySet().stream().collect(Collectors.joining(","));
+        String value = data.values().stream().collect(Collectors.joining(","));
+        List<String> list = Arrays.asList(headers, value);
+
+
+        try {
+            Files.write(Paths.get("mt103.csv"), list, utf8);
+            System.out.println("File Generated with below data");
+            data.forEach((key, val) -> {
+                System.out.println(key + " : " + val);
+            });
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }"catchmasale)), target aqire
+
+
+
+
+
+
+                00
+                        www.google.com
+                                trying,tried
+
     }
+
 }
